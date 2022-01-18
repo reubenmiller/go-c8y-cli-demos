1ac3fc9278b4db417f24c09cfdaa55e3dbf11773
Reuben Miller <reuben.miller@softwareag.com>
2022-01-18 22:08:02 +0100

    use piped input to prevent ci/cd stdin redirection problem

--- 2022-01-27/02-cicd/deploy-webapps.sh
+++ 2022-01-27/02-cicd/deploy-webapps.sh
@@ -4,6 +4,7 @@ echo "Deploying an web application"
 find . -type d -name "*web*" -exec c8y applications createHostedApplication --file "{}/" \;
 
 echo "Cleanup: Keeping last 3 deployed applications"
-c8y applications listApplicationBinaries --id mywebapp -p 100 \
+echo "mywebapp" \
+    | c8y applications listApplicationBinaries -p 100 \
     | head -n -3 \
     | c8y applications deleteApplicationBinary --application mywebapp
