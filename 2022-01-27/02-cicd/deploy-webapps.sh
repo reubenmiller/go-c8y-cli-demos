f9644eac432c0de64432c3cb8ab7936cf1788efc
Reuben Miller <reuben.miller@softwareag.com>
2022-01-18 22:38:19 +0100

    add option to control how many deployments to keep

--- 2022-01-27/02-cicd/deploy-webapps.sh
+++ 2022-01-27/02-cicd/deploy-webapps.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+keep_last=5
+
+if [ "$#" -gt 0 ]; then
+    keep_last="$1"
+fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd "$SCRIPT_DIR" || exit 1
@@ -7,13 +12,9 @@ deploy_app () {
     local name="$1"
 
     echo "[name=$name] Deploying an web application (keeping only last 3 deployments)"
-    # c8y applications createHostedApplication -n --file "$name/"
-
-    # echo "[name=$name] Cleanup: Keeping last 3 deployed applications"
-    # echo "$name" \
     c8y applications createHostedApplication -n --file "$name/" \
         | c8y applications listApplicationBinaries -p 100 \
-        | head -n -3 \
+        | head -n "-$keep_last" \
         | c8y applications deleteApplicationBinary --application "$name"
 }
 
