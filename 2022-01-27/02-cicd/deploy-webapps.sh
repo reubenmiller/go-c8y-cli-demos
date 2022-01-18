83e731a32dab781fc00a0b295cc64ac6ea4fd34c
Reuben Miller <reuben.miller@softwareag.com>
2022-01-18 22:31:14 +0100

    refactor deployment

--- 2022-01-27/02-cicd/deploy-webapps.sh
+++ 2022-01-27/02-cicd/deploy-webapps.sh
@@ -6,12 +6,12 @@ pushd "$SCRIPT_DIR" || exit 1
 deploy_app () {
     local name="$1"
 
-    echo "[name=$name] Deploying an web application"
-    # find . -type d -name "*web*" -exec c8y applications createHostedApplication --file "{}/" \;
-    c8y applications createHostedApplication -n --file "$name/"
+    echo "[name=$name] Deploying an web application (keeping only last 3 deployments)"
+    # c8y applications createHostedApplication -n --file "$name/"
 
-    echo "[name=$name] Cleanup: Keeping last 3 deployed applications"
-    echo "$name" \
+    # echo "[name=$name] Cleanup: Keeping last 3 deployed applications"
+    # echo "$name" \
+    c8y applications createHostedApplication -n --file "$name/" \
         | c8y applications listApplicationBinaries -p 100 \
         | head -n -3 \
         | c8y applications deleteApplicationBinary --application "$name"
