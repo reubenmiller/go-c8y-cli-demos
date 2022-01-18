6241e4fa33928ec48ec67cd7284a38634b0affbe
Reuben Miller <reuben.miller@softwareag.com>
2022-01-18 22:18:43 +0100

    change working directory

--- 2022-01-27/02-cicd/deploy-webapps.sh
+++ 2022-01-27/02-cicd/deploy-webapps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd "$SCRIPT_DIR" || exit 1
+
 echo "Deploying an web application"
 find . -type d -name "*web*" -exec c8y applications createHostedApplication --file "{}/" \;
 
