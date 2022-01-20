b438c6d9d2669658913bd921cc9113c67d0849a6
Reuben Miller <reuben.miller@softwareag.com>
2022-01-20 08:38:03 +0100

    fix template error

--- 2022-01-27/02-cicd/config/deploy-config.sh
+++ 2022-01-27/02-cicd/config/deploy-config.sh
@@ -10,6 +10,15 @@ if ! declare -p GITHUB_RUN_NUMBER 2>/dev/null; then
     GITHUB_RUN_NUMBER=0
 fi
 
+if ! declare -p GITHUB_REF_NAME 2>/dev/null; then
+    GITHUB_REF_NAME=0
+fi
+
+if ! declare -p GITHUB_SHA 2>/dev/null; then
+    GITHUB_SHA=0
+fi
+
+
 echo "Removing previous configuration files"
 c8y configuration list --query "configurationType eq 'DEVICE_AGENT' and has(cicd_build)" \
 | c8y configuration delete
@@ -20,5 +29,5 @@ for file in *agent*.ini; do
         c8y configuration create --name "${file%.*}" \
             --configurationType "DEVICE_AGENT" \
             --file "$file" \
-            --template "{description: 'Agent config (branch: $GITHUB_REF_NAME, run: $GITHUB_RUN_NUMBER)', cicd_build: {branch: '$GITHUB_REF_NAME', sha: '$GITHUB_SHA' runId: '$GITHUB_RUN_ID', runNumber: '$GITHUB_RUN_NUMBER'}}"
+            --template "{description: 'Agent config (branch: $GITHUB_REF_NAME, run: $GITHUB_RUN_NUMBER)', cicd_build: {branch: '$GITHUB_REF_NAME', sha: '$GITHUB_SHA', runId: '$GITHUB_RUN_ID', runNumber: '$GITHUB_RUN_NUMBER'}}"
 done
