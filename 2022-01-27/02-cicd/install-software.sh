d64015a42cd8c5544adfae1f766dcb03a038a756
Reuben Miller <reuben.miller@softwareag.com>
2022-01-26 20:40:50 +0100

    adding logging to install script

--- 2022-01-27/02-cicd/install-software.sh
+++ 2022-01-27/02-cicd/install-software.sh
@@ -2,7 +2,6 @@
 
 # bash options to fail fast
 set -euo pipefail
-DEPLOY_GROUP="${1:-"canary_10"}"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd "$SCRIPT_DIR" || exit 1
@@ -12,6 +11,7 @@ install_software () {
     local name="$2"
     local version="$3"
 
+    echo "Install software: name=$name, version=$version, deployment_group=$group_name"
     c8y devices list -n --includeAll --query "c8y_DeploymentGroup.name eq '$group_name'" \
     | c8y software versions install --software "$name" --version "$version"
 }
