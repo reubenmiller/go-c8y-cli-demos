81c4ba4f6629331bfbf384b28ac2240574143bc4
Reuben Miller <reuben.miller@softwareag.com>
2022-01-26 20:52:40 +0100

    adding debug statement to show the devices

--- 2022-01-27/02-cicd/install-software.sh
+++ 2022-01-27/02-cicd/install-software.sh
@@ -11,6 +11,12 @@ install_software () {
     local name="$2"
     local version="$3"
 
+    echo "Devices"
+    c8y devices list -n --includeAll --query "c8y_DeploymentGroup.name eq '$group_name'" --output table
+
+    echo "Activity log settings"
+    c8y settings list --select activitylog --raw 
+
     echo "Install software: name=$name, version=$version, deployment_group=$group_name"
     c8y devices list -n --includeAll --query "c8y_DeploymentGroup.name eq '$group_name'" \
     | c8y software versions install --software "$name" --version "$version"
@@ -19,4 +25,9 @@ install_software () {
 #
 # Main
 #
-install_software "$1" "$2" "$3"
+
+# DEBUG: check sort version
+sort --version
+
+LATEST_VERSION=$( c8y software versions list -n --software python3-c8ylp --includeAll --select "*.version" -o csv | sort -Vr | head -1 )
+install_software "$1" "$2" "$LATEST_VERSION"
