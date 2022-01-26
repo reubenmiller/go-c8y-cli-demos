590f3744263398a86c5a7c40d632935fc07976fa
Reuben Miller <reuben.miller@softwareag.com>
2022-01-26 20:19:19 +0100

    add install software build step

---
+++ 2022-01-27/02-cicd/install-software.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# bash options to fail fast
+set -euo pipefail
+DEPLOY_GROUP="${1:-"canary_10"}"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd "$SCRIPT_DIR" || exit 1
+
+install_software () {
+    local group_name="$1"
+    local name="$2"
+    local version="$3"
+
+    c8y devices list -n --includeAll --query "c8y_DeploymentGroup.name eq '$group_name'" \
+    | c8y software versions install --software "$name" --version "$version" \
+    | c8y operations | wait
+}
+
+#
+# Main
+#
+install_software "$1" "$2" "$3"
