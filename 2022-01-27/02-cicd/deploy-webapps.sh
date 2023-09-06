3bccd8cc2a823603cc0ef76c941d54f8a881c90d
Reuben Miller <reuben.d.miller@gmail.com>
2023-09-06 12:10:07 +1000

    chore(gha): use github action to install go-c8y-cli

--- 2022-01-27/02-cicd/deploy-webapps.sh
+++ 2022-01-27/02-cicd/deploy-webapps.sh
@@ -15,7 +15,7 @@ pushd "$SCRIPT_DIR" || exit 1
 deploy_app () {
     local name="$1"
 
-    echo "[name=$name] Deploying an web application (keeping only last 3 deployments)"
+    echo "[name=$name] Deploying an web application (keeping only last $keep_last deployments)"
     c8y applications createHostedApplication --file "$name/" \
         | c8y applications listApplicationBinaries -p 100 \
         | head -n "-$keep_last" \
